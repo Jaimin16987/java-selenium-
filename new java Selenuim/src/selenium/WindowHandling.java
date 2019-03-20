5c3d48e32943184f8433f12ac29d502a47d6c5b2
patel <patel@DESKTOP-631K77G>
2019-03-20 16:52:25 -0500

    added some commit

--- new java Selenuim/src/selenium/WindowHandling.java
+++ new java Selenuim/src/selenium/WindowHandling.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WindowHandling {
-// this is not complete project
+// this is not complete project.
 	public static void main(String[] args) throws Exception {
 		System.setProperty("webdriver.chrome.driver","C:\\Driver\\chromedriver.exe" );
 		WebDriver Driver = new ChromeDriver();
