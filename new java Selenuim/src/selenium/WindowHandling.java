61edf52d75f061f0d12823903e8bd9a6df313c7d
patel <patel@DESKTOP-631K77G>
2019-03-20 16:43:27 -0500

    new commit add

--- new java Selenuim/src/selenium/WindowHandling.java
+++ new java Selenuim/src/selenium/WindowHandling.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WindowHandling {
-
+// thi is not complete project
 	public static void main(String[] args) throws Exception {
 		System.setProperty("webdriver.chrome.driver","C:\\Driver\\chromedriver.exe" );
 		WebDriver Driver = new ChromeDriver();
@@ -32,6 +32,7 @@ public class WindowHandling {
 	 Driver.switchTo().window(perentWindowId);
 	 Thread.sleep(2000);
 	 System.out.println("perent window title is "+Driver.getTitle());
+	 
 	 Driver.close();
 	
 	
